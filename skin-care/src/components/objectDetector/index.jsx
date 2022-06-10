ac19771cc4e0bc5338ab29d94a870f9bb5886ca5
Fernando Quiroga <fquiroga@info-arch.com>
2022-06-10 00:41:54 -0400

    react project

---
+++ skin-care/src/components/objectDetector/index.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+
+import "@tensorflow/tfjs-backend-cpu";
+import "@tensorflow/tfjs-backend-webgl";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+
+const ObjectDetectorContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const DetectorContainer = styled.div`
+    min-width: 200px;
+    height: 500px;
+    border: 3px solid #fff;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+`;
+
+const TargetImg = styled.img`
+    height: 100%;
+`;
+
+const HiddenFileInput = styled.input`
+    display: none;
+`;
+
+const SelectButton = styled.button`
+    padding: 7px 10px;
+    border: 2px solid transparent;
+    background-color: #fff;
+    color: black;
+    font-size: 16px;
+    font-weight: 500;
+    outline: none;
+    margin-top: 2em;
+    cursor: pointer;
+    transition: all 260ms ease-in-ot;
+
+    &:hover {
+        background-color: transparent;
+        border: 2px solid #fff;
+        color: #fff;
+    }
+`;
+
+export function ObjectDetector(props) {
+
+    return (
+        <ObjectDetectorContainer>
+            <DetectorContainer>Img</DetectorContainer>
+            <SelectButton>Select Image</SelectButton>
+        </ObjectDetectorContainer>
+    );
+}
